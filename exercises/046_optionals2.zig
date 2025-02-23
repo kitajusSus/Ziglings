d0bd73051c99774366fb502d574674962ccb6cfe
kitajuSus <kitaj2458@gmail.com>
2025-02-23 20:46:41 +0100

    znajduje juz pliki js i html

--- exercises/046_optionals2.zig
+++ exercises/046_optionals2.zig
@@ -1,4 +1,6 @@
+// the problem is that I don't know how to stop the loop when the tail is null or undefined
 //
+// Exercise:
 // Now that we have optional types, we can apply them to structs.
 // The last time we checked in with our elephants, we had to link
 // all three of them together in a "circle" so that the last tail
@@ -22,8 +24,9 @@ const std = @import("std");
 
 const Elephant = struct {
     letter: u8,
-    tail: *Elephant = null, // Hmm... tail needs something...
-    visited: bool = false,
+    tail: ?*Elephant = null, // Hmm... tail needs something...
+    //* ten element pokazuje ze z definicji moze tu być cokolwiek, numer, string, czy inne struktuyry itd.
+    visited: bool = false, // klasyka, odwiedzony czy nie,
 };
 
 pub fn main() void {
@@ -34,11 +37,11 @@ pub fn main() void {
     // Link the elephants so that each tail "points" to the next.
     linkElephants(&elephantA, &elephantB);
     linkElephants(&elephantB, &elephantC);
-
+    linkElephants(&elephantC, null);
     // `linkElephants` will stop the program if you try and link an
     // elephant that doesn't exist! Uncomment and see what happens.
-    // const missingElephant: ?*Elephant = null;
-    // linkElephants(&elephantC, missingElephant);
+    //const missingElephant: ?*Elephant = null;
+    //linkElephants(&elephantC, missingElephant);
 
     visitElephants(&elephantA);
 
@@ -48,9 +51,9 @@ pub fn main() void {
 // If e1 and e2 are valid pointers to elephants,
 // this function links the elephants so that e1's tail "points" to e2.
 fn linkElephants(e1: ?*Elephant, e2: ?*Elephant) void {
-    e1.?.tail = e2.?;
+    // If either elephant is missing, stop the program.
+    e1.?.*.tail = e2;
 }
-
 // This function visits all elephants once, starting with the
 // first elephant and following the tails to the next elephant.
 fn visitElephants(first_elephant: *Elephant) void {
@@ -66,6 +69,6 @@ fn visitElephants(first_elephant: *Elephant) void {
 
         // HINT: We want something similar to what `.?` does,
         // but instead of ending the program, we want to exit the loop...
-        e = e.tail ???
+        e = e.tail orelse break;
     }
 }

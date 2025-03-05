87ac406ac5ca271a749b9d1df0806833a1c5450b
kitajuSus <kitaj2458@gmail.com>
2025-03-05 21:16:14 +0100

    skibidi

--- exercises/048_methods2.zig
+++ exercises/048_methods2.zig
@@ -54,7 +54,9 @@ fn visitElephants(first_elephant: *Elephant) void {
 
         // This gets the next elephant or stops:
         // which method do we want here?
-        e = if (e.hasTail()) e.??? else break;
+        e = if (e.hasTail()) e.getTail() else break;
+        // metoda którą dałem na tył czyli jesli hasTail == True.
+        // trzeba było e.getTail() zamiast e.???
     }
 }
 

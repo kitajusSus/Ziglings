5d0066723470627a5597d693eef99a493cb9f1fd
kitajusSusWWA <kitaj2458@gmail.com>
2025-09-28 19:20:04 +0200

    new

--- exercises/084_async.zig
+++ exercises/084_async.zig
@@ -48,11 +48,12 @@ const print = @import("std").debug.print;
 pub fn main() void {
     // Additional Hint: you can assign things to '_' when you
     // don't intend to do anything with them.
-    foo();
+    _ = foo();
 }
 
 fn foo() void {
     print("foo() A\n", .{});
     suspend {}
+
     print("foo() B\n", .{});
 }
